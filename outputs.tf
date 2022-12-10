a82928413e923615e06cd4f4f308ef29bac74aa9
jeff buenting <jeffbuenting@gmail.com>
2022-12-10 18:37:51 -0500

    changed instance name

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "password" {
   description = "Local Admin Password"
   value = [
-    for g in aws_instance.unica_servers : rsadecrypt(g.password_data, file(var.private_key))
+    for g in aws_instance.instance : rsadecrypt(g.password_data, file(var.private_key))
   ]
 
 }
