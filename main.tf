b7459d42b7cfe7a291bc2a539b3a07a870570465
jeff buenting <jeffbuenting@gmail.com>
2022-12-13 11:11:10 -0500

    removed kp name hard code

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Terraform module to create an EC2 Insance
 
 data "aws_key_pair" "kp" {
-  # key_name           = var.kp_name
-  key_name           = "InstanceKey"
+  key_name = var.kp_name
+  #key_name           = "InstanceKey"
   include_public_key = true
 }
 
